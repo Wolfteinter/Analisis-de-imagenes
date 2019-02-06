7db7c3a8787b29a23cabee82841a040ee2e0ca7b
Roberto Cruz Leija <robanac@gmail.com>
2019-02-06 15:45:13 -0600

    -se agrega la generacion de instancias en grises con comentarios

--- src/muestreo/EscalaGrises.java
+++ src/muestreo/EscalaGrises.java
@@ -21,9 +21,13 @@ public class EscalaGrises {
      // recorrido por pixel para hacer el tono gris
      for(int x=0; x<bi.getWidth();x++){
          for(int y=0;y<bi.getHeight();y++){
+         //extrer el tono del pixel en RGB
          Color color = new Color(bi.getRGB(x, y));
+         // calculamos la reducción por promedio
          int gris = (color.getRed()+color.getGreen()+color.getBlue())/3;
+         // generamos el color con el nuevo tono promedio(gris)
          color = new Color(gris,gris,gris);
+         // se modifica el RGB del pixel en base al nuevo tono
          bi.setRGB(x, y, color.getRGB());
          }
      }
